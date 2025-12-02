6b992d42c139d5697f27839fda144bdda99c0f7c
Tankun888 <taenkhun.t@kkumail.com>
2025-12-02 10:56:50 +0700

    Tankun

---
+++ Tankun/Tankun/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tankun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print "Hello World!" to the console
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is, Tankun.");
+            Console.WriteLine("stdentbID 683456664-0");
+            Console.WriteLine("major: Computer Science");
+            Console.WriteLine("Faculty : Is ");
+            Console.WriteLine("KKU");
+     
+        }
+    }
+}
