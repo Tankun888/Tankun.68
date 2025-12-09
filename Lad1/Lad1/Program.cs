c6bd0eb4e69dca3c7f14b45cdbb3b6f2e4f70022
Tankun888 <taenkhun.t@kkumail.com>
2025-12-09 11:33:53 +0700

    tankun
    683450064-0

---
+++ Lad1/Lad1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lad1
+{
+    internal class Program
+    {
+        private static string year;
+
+        static void Main(string[] args)
+        {
+            Console.Write("อายุ: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int birthYear = DateTime.Now.Year - age;
+
+            string gen =
+                birthYear <= 1980 ? "Gen X" :
+                birthYear <= 1996 ? "Gen Y" :
+                birthYear <= 2012 ? "Gen Z" :
+                "Gen Alpha";
+
+            Console.WriteLine($"เกิดปี {birthYear}, คุณเป็น {gen}");
+
+        }
+
+    }
+}
